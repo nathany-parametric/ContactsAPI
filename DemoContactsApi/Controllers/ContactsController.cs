57498afe82ddcf6c5123d0000bb9f0571c997d8b
Nathan Yang <nyang@paraport.com>
2019-10-15 10:00:04 -0700

    First commit

---
+++ DemoContactsApi/Controllers/ContactsController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoContactsApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private int _contactId = 3;
+        List<Contact> contacts = new List<Contact>
+        {
+            new Contact
+            {
+                Id = 1,
+                Name = "Bob",
+                Email = "bob@bob.com"
+            },
+            new Contact
+            {
+                Id = 2,
+                Name = "Bill",
+                Email = "bill@bill.com"
+            }
+        };
+
+
+        // GET: api/Contacts
+        [HttpGet]
+        [ProducesResponseType(200), ProducesResponseType(404)]
+        public ActionResult<IEnumerable<Contact>> Get()
+        {
+            return contacts;
+
+        }
+
+        // GET: api/Contacts/5
+        [HttpGet("{id}", Name = "Get")]
+        [ProducesResponseType(200), ProducesResponseType(404)]
+        public ActionResult<Contact> GetById(int id)
+        {
+            return contacts.SingleOrDefault(c => c.Id == id);
+        }
+
+        // POST: api/Contacts
+        [HttpPost]
+        [ProducesResponseType(200), ProducesResponseType(404)]
+        public ActionResult<Contact> Post([FromBody] Contact contact)
+        {
+            contact.Id = _contactId++;
+            contacts.Add(contact);
+
+            return contact;
+        }
+
+        // PUT: api/Contacts/5
+        [HttpPut("{id}")]
+        [ProducesResponseType(200), ProducesResponseType(404)]
+        public ActionResult<Contact> Put(int id, [FromBody] Contact contact)
+        {
+            var existingContact = contacts.SingleOrDefault(c => c.Id == id);
+
+            if (existingContact != null)
+            {
+                existingContact.Email = contact.Email;
+                existingContact.Name = contact.Name;
+            }
+
+            return existingContact;
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        [ProducesResponseType(200), ProducesResponseType(404)]
+        public bool Delete(int id)
+        {
+            var deleted = contacts.Remove(contacts.SingleOrDefault(c => c.Id == id));
+            return deleted;
+        }
+    }
+}
+//dotnet publish --configuration Release
---
+++ DemoContactsApi/Startup.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace DemoContactsApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("contacts-v1", new Info
+                {
+                    Title = "Contacts API Swagger",
+                    Version = "1.0.0"
+                });
+            });
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/contacts-v1/swagger.json", "Contacts API Swagger");
+                }
+            );
+        }
+    }
+}
